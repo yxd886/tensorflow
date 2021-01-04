f0b1ecdf49e6a7d3ae862cb53e6887b1a3bfb2f0
Yi Xiaodong <409070169@qq.com>
2021-01-04 11:04:14 +0800

    dump hlo after tensor fusion

--- tensorflow/compiler/xla/service/gpu/gpu_compiler.cc
+++ tensorflow/compiler/xla/service/gpu/gpu_compiler.cc
@@ -358,6 +358,8 @@ Status GpuCompiler::OptimizeHloModule(
        /*combine_threshold_count=*/1000000);
     TF_RETURN_IF_ERROR(pipeline.Run(hlo_module).status());
 	////cout<<"out tensor fusion pass"<<endl;
+    DumpHloModuleIfEnabled(*hlo_module,
+                           "after_all_reduce_combiner");
 
   }
   {
